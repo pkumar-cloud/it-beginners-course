7a55c6665b31945106bf6bc0ca4b8191664690b5
Nana Janashia <nn_janashia@yahoo.com>
2022-10-26 19:07:08 +0200

    add mongodb connection to backend app

--- server.js
+++ server.js
@@ -1,26 +1,60 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
+
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+const dbName = 'company_db'
+const collName = 'employees'
 
 app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/dist'))
 
-app.get('/get-profile', function(req, res) {
+app.get('/get-profile', async function(req, res) {
+    // connect to mongodb database
+    await client.connect()
+    console.log('Connected successfully to server')
+
+    // initiate or get the db & collection
+    const db = client.db(dbName)
+    const collection = db.collection(collName)
+    
+    // get data from database
+    const result = await collection.findOne({id: 1})
+    console.log(result)
+    client.close()
+
     const response = {
-        name: "Anna Smith",
-        email: "anna.smith@example.com",
-        interests: "coding"
+        name: result.name,
+        email: result.email,
+        interests: result.interests
     }
+
     res.send(response)
 })
 
-app.post('/update-profile', function(req, res) {
+app.post('/update-profile', async function(req, res) {
     const payload = req.body
     console.log(payload)
+
     if (Object.keys(payload).length === 0) {
         res.send({error: "empty payload. Couldn't update user profile data"})
     } else {
-        // updating user profile
+        // connect to mongodb database
+        await client.connect()
+        console.log('Connected successfully to database server')
+
+        // initiate or get the db & collection
+        const db = client.db(dbName)
+        const collection = db.collection(collName)
+
+        // save payload data to the database
+        payload['id'] = 1;
+        const updatedValues = { $set: payload }
+        await collection.updateOne({id: 1}, updatedValues, {upsert: true});
+        client.close()
+
         res.send({info: "user profile data updated successfully"})
     }
 })
